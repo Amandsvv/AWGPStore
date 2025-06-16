7dafee3e26a2c5fc10e17f757991405387aac4c3
Aman Kumar <amandsvv4006@gmail.com>
2025-06-16 16:54:38 +0530

    responsive layout

--- JS/main.js
+++ JS/main.js
@@ -17,3 +17,44 @@ document.querySelector(".User-icon").addEventListener('mouseleave', () =>{
     const userBox = document.querySelector(".user-icon-box");
     userBox.style.display = "none";
 });
+
+
+document.querySelector(".product-btn").addEventListener('click', () =>{
+    const userBox = document.querySelector(".products-box");
+
+    if(userBox.style.display === "none"){
+        userBox.style.display = "flex";
+    }else{
+        userBox.style.display ="none"
+    }
+});
+
+document.querySelector(".product-btn").addEventListener('mouseover', () =>{
+    const userBox = document.querySelector(".products-box");
+    userBox.style.display = "flex";
+});
+
+document.querySelector(".product-btn").addEventListener('mouseleave', () =>{
+    const userBox = document.querySelector(".products-box");
+    userBox.style.display = "none";
+});
+
+document.querySelector(".otherlink-btn").addEventListener('click', () =>{
+    const userBox = document.querySelector(".otherlink-box");
+
+    if(userBox.style.display === "none"){
+        userBox.style.display = "flex";
+    }else{
+        userBox.style.display ="none"
+    }
+});
+
+document.querySelector(".otherlink-btn").addEventListener('mouseover', () =>{
+    const userBox = document.querySelector(".otherlink-box");
+    userBox.style.display = "flex";
+});
+
+document.querySelector(".otherlink-btn").addEventListener('mouseleave', () =>{
+    const userBox = document.querySelector(".otherlink-box");
+    userBox.style.display = "none";
+});
